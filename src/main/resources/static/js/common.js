2bd6d130c2fe12a28074e2633604e3299e39b909
iwamih31 <iwamih31@gmail.com>
2023-11-28 00:15:31 +0900

    common.js修正

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -2,12 +2,10 @@
  *
  */
 
-var n3 = document.getElementsByClassName('num3');
+ var n3 = document.getElementsByClassName('num3');
 for (var i = 0; n3.length; i++){
 	let p = n3[i].textContent.replace('\xA5', '');
-	if ('' != p){
-		if(isFinite(p)){
-			n3[i].innerHTML = Number(p).toLocaleString('ja-JP', {"style":"currency", "currency":"JPY"});
-		}
+	if(isFinite(p)){
+		n3[i].innerHTML = Number(p).toLocaleString('ja-JP', {"style":"currency", "currency":"JPY"});
 	}
 }
